e4100dc6328de7cf258f1a04e4294f9ee5bcec3f
gaatjeniksaan <barendlinders@gmail.com>
2016-08-04 15:02:32 +0200

    Add files via upload

---
+++ Generate list primes smaller than n.py
@@ -0,0 +1,10 @@
+def iprimes_upto(limit):
+    is_prime = [True] * limit
+    for n in range(2, limit):
+        if is_prime[n]:
+           yield n
+           for i in range(n*n, limit, n): # start at ``n`` squared
+               is_prime[i] = False
+
+
+print(list(iprimes_upto(100000)))
